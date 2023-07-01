5cbc421447c5e77692287ff24b3362e3cb513e14
Brian <fisetbrian@gmail.com>
2023-07-01 15:13:43 -0400

    Add function to play on console

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const computer = getComputerChoice();
 // create a function that generates a random selection for the computer
 function getComputerChoice() {
 // generate a number 1 - 3
-    let number = Math.floor(Math.random() * 3) + 1
+    let number = Math.floor(Math.random() * 3) + 1;
 // use number to choice rock paper or scissors
     switch(number){
         case 1:
@@ -20,3 +20,28 @@ function getComputerChoice() {
 // return choice
     return number
 }
+
+// create a function that thats two arguments computer choice and player choice
+function playGame(playerChoice, computerChoice) {
+    // create a win variable
+    let win = false;
+    // check  if playerChoice and computer choice are the same
+    if(playerChoice === computerChoice) {
+        // say that they tied
+        console.log('You have Tied');
+        //if not the same compare choice to choose winner
+    } else if(playerChoice === 'Rock' && computerChoice === 'Paper' ||
+        playerChoice === 'Paper' && computerChoice === 'Scissors' ||
+        playerChoice === 'Scissors' && computerChoice === 'Rock'){
+        win = true;
+        console.log('Computer has Won');
+    } else if(computerChoice === 'Rock' && playerChoice === 'Paper' ||
+    computerChoice === 'Paper' && playerChoice === 'Scissors' ||
+    computerChoice === 'Scissors' && playerChoice === 'Rock'){
+        win = true;
+        console.log('Player has Won');
+        // if error in choice trow error
+    } else {
+        console.warn('Error Value not Accepted')
+    };
+};
