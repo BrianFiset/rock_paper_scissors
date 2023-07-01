a0590dc86e694b1faef207bf12b1beca469b6eee
Brian <fisetbrian@gmail.com>
2023-07-01 15:44:54 -0400

    Let player choice be case insensitiveand change computer choice at end of game

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // player choice
 const playerSelection = "Rock";
 // computer choice
-const computer = getComputerChoice();
+let computer = getComputerChoice();
 // create a function that generates a random selection for the computer
 function getComputerChoice() {
 // generate a number 1 - 3
@@ -23,6 +23,8 @@ function getComputerChoice() {
 
 // create a function that thats two arguments computer choice and player choice
 function playGame(playerChoice, computerChoice) {
+    // make player choice character insensitive 
+    playerChoice = capitalizeFirstLetter(playerChoice)
     // create a win variable
     let win = false;
     // check  if playerChoice and computer choice are the same
@@ -34,14 +36,21 @@ function playGame(playerChoice, computerChoice) {
         playerChoice === 'Paper' && computerChoice === 'Scissors' ||
         playerChoice === 'Scissors' && computerChoice === 'Rock'){
         win = true;
-        console.log('Computer has Won');
+        console.log(`You Lose! ${computerChoice} beats ${playerChoice}`);
     } else if(computerChoice === 'Rock' && playerChoice === 'Paper' ||
     computerChoice === 'Paper' && playerChoice === 'Scissors' ||
     computerChoice === 'Scissors' && playerChoice === 'Rock'){
         win = true;
-        console.log('Player has Won');
+        console.log(`You Win! ${playerChoice} beats ${computerChoice}`);
         // if error in choice trow error
     } else {
         console.warn('Error Value not Accepted')
     };
+    computer = getComputerChoice()
 };
+
+// create function to make text lower case except first letter
+function capitalizeFirstLetter(word){
+    word = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+    return word
+}
